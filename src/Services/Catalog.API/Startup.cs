7a6c0836e304bd7fddf7ab9f307c497c078cfdb8
JavierJH99 <JavierJH99@users.noreply.github.com>
2021-11-17 17:51:46 +0100

    conexión bbdd lista

--- src/Services/Catalog.API/Startup.cs
+++ src/Services/Catalog.API/Startup.cs
@@ -1,6 +1,8 @@
+using Catalog.API.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -31,6 +33,9 @@ namespace Catalog.API
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "Catalog.API", Version = "v1" });
             });
+            services.AddDbContext<CatalogContext>(options => 
+            options.UseSqlServer(
+                Configuration.GetConnectionString("DefaultConnection")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
