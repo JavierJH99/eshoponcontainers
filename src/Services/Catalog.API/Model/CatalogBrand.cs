2c5bea1bb09f62d9b2393c8cefdcab263857a5c4
JavierJH99 <JavierJH99@users.noreply.github.com>
2021-11-17 17:30:07 +0100

    Crear Modelo

---
+++ src/Services/Catalog.API/Model/CatalogBrand.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Catalog.API.Model
+{
+    public class CatalogBrand
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        [StringLength(100)]
+        public string Brand { get; set; }
+    }
+}
---
+++ src/Services/Catalog.API/Model/CatalogItem.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Catalog.API.Model
+{
+    public class CatalogItem
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        [Required]
+        public decimal Price { get; set; }
+
+        [ForeignKey("CatalogTypeId")]
+        public CatalogType CatalogType { get; set; }
+
+        [ForeignKey("CatalogBrandId")]
+        public CatalogBrand CatalogBrand { get; set; }
+
+        // Quantity in stock
+        public int AvailableStock { get; set; }
+
+        // Available stock at which we should reorder
+        public int RestockThreshold { get; set; }
+
+        // Maximum number of units that can be in-stock at any time
+        //(due to physical/logistical constraints in warehouses)
+        public int MaxStockThreshold { get; set; }
+
+        /// <summary>
+        /// True if item is on reorder
+        /// </summary>
+        public bool OnReorder { get; set; }
+
+        public CatalogItem() { }
+    }
+}
+
